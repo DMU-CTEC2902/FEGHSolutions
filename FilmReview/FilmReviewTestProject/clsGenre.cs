f4b4b6578041735fe714a0b781e8e25684174f8d
P17177241 <P17177241@GWHXX653WIN003W>
2019-03-28 12:48:25 +0000

    complete testing for cls Genre

---
+++ FilmReview/FilmReviewTestProject/clsGenre.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace FilmReviewTestProject
+{
+    public class clsGenre
+    {
+        //priavet data memeber for the GenreName property
+        private string mGenreName;
+
+        public string GenreName
+        {
+            get
+            {
+                //return the private data
+                return mGenreName;
+            }
+            set
+            {
+                //set the private data 
+                mGenreName = value;
+            }
+        }
+
+        public bool Valid(string GenreName, string Age)
+        {
+            // boolean variable
+            Boolean Ok = true;
+
+            if (GenreName.Length == 2)
+            {
+                Ok = false;
+            }
+            if (GenreName.Length < 3)
+            {
+                Ok = false;
+            }
+            if (GenreName.Length > 60)
+            {
+                Ok = false;
+            }
+
+            return Ok;
+
+
+
+        }
+
+    }
+}
---
+++ FilmReview/FilmReviewTestProject/tstGenre.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FilmReviewTestProject;
+
+namespace GenreReviewTestProject
+{
+    /// <summary>
+    /// Summary description for Genre
+    /// </summary>
+    [TestClass]
+    public class Genre
+    {
+        public Genre()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create 
+            clsGenre AnGenre = new clsGenre();
+            //test to see that it exists
+            Assert.IsNotNull(AnGenre);
+        }
+
+        [TestMethod]
+        public void GenreNameProperty()
+        {
+            //create an instance of the class we want to create 
+            clsGenre AnGenre = new clsGenre();
+            //create some test data to assign to the property
+            string TestData = "comedy";
+            //assign the data to the proerty
+            AnGenre.GenreName = TestData;
+            //test to see that the two values are the same 
+            Assert.AreEqual(AnGenre.GenreName, TestData);
+        }
+
+       
+
+
+        [TestMethod]
+        public void ValidMethodOK()
+        {
+            //create an instance of the class we want to create 
+            clsGenre AGenre = new clsGenre();
+            //boolean variable to store the result of the validation
+            Boolean Ok = false;
+            //create some data to use with the method
+            string GenreName = "Bob";
+            string Age = "23";
+            //invoke the method
+            Ok = AGenre.Valid(GenreName, Age);
+            //test tto see that the reuslt is correct
+            Assert.IsTrue(Ok);
+        }
+    }
+
+}
