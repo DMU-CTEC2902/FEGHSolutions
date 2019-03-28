5ab96eede6889396782e7fad6f01edc39698c62e
P17177241 <P17177241@GWHXX653WIN003W>
2019-03-28 12:40:29 +0000

    Add tst Review

---
+++ FilmReview/FilmReviewTestProject/clsReviews.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace FilmReviewTestProject
+{
+    public class clsReviews
+    {
+        //priavet data memeber for the Title property
+        private string mTitle;
+
+        //priavet data memeber for the MainReview property
+        private string mMainReview;
+
+        //priavet data memeber for the Score property
+        private string mScore;
+
+        public string Title
+        {
+            get
+            {
+                //return the private data
+                return mTitle;
+            }
+            set
+            {
+                //set the private data 
+                mTitle = value;
+            }
+        }
+        public string MainReview
+
+        {
+            get
+            {
+                //return the private data
+                return mMainReview;
+            }
+            set
+            {
+                //set the private data 
+                mMainReview = value;
+            }
+        }
+        public string Score
+        {
+            get
+            {
+                //return the private data
+                return mScore;
+            }
+            set
+            {
+                //set the private data 
+                mScore = value;
+            }
+        }
+
+        public bool Valid(string Title, string MainReview, string Score)
+        {
+            // boolean variable
+            Boolean Ok = true;
+
+            if (Title.Length == 2)
+            {
+                Ok = false;
+            }
+            if (Title.Length < 3)
+            {
+                Ok = false;
+            }
+            if (Title.Length > 60)
+            {
+                Ok = false;
+            }
+
+            if (MainReview.Length == 2)
+            {
+                Ok = false;
+            }
+            if (MainReview.Length < 3)
+            {
+                Ok = false;
+            }
+            if (MainReview.Length > 60)
+            {
+                Ok = false;
+            }
+
+            if (Score.Length == 2)
+            {
+                Ok = false;
+            }
+            if (Score.Length < 3)
+            {
+                Ok = false;
+            }
+            if (Score.Length > 60)
+            {
+                Ok = false;
+            }
+
+
+            return Ok;
+
+
+
+        }
+
+    }
+}
---
+++ FilmReview/FilmReviewTestProject/tstReviews.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FilmReviewTestProject;
+
+namespace ReviewsReviewTestProject
+{
+    /// <summary>
+    /// Summary description for Reviews
+    /// </summary>
+    [TestClass]
+    public class Reviews
+    {
+        public Reviews()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create 
+            clsReviews AnReviews = new clsReviews();
+            //test to see that it exists
+            Assert.IsNotNull(AnReviews);
+        }
+
+        [TestMethod]
+        public void TitleProperty()
+        {
+            //create an instance of the class we want to create 
+            clsReviews AnReviews = new clsReviews();
+            //create some test data to assign to the property
+            string TestData = "Haha";
+            //assign the data to the proerty
+            AnReviews.Title = TestData;
+            //test to see that the two values are the same 
+            Assert.AreEqual(AnReviews.Title, TestData);
+        }
+
+        [TestMethod]
+        public void MainReviewProperty()
+        {
+            //create an instance of the class we want to create 
+            clsReviews AnReviews = new clsReviews();
+            //create some test data to assign to the property
+            string TestData = "great";
+            //assign the data to the proerty
+            AnReviews.MainReview = TestData;
+            //test to see that the two values are the same 
+            Assert.AreEqual(AnReviews.MainReview, TestData);
+        }
+
+        [TestMethod]
+        public void ScoreProperty()
+        {
+            //create an instance of the class we want to create 
+            clsReviews AnReviews = new clsReviews();
+            //create some test data to assign to the property
+            string TestData = "3.3";
+            //assign the data to the proerty
+            AnReviews.Score = TestData;
+            //test to see that the two values are the same 
+            Assert.AreEqual(AnReviews.Score, TestData);
+        }
+
+        [TestMethod]
+        public void ValidMethodOK()
+        {
+            //create an instance of the class we want to create 
+            clsReviews AReviews = new clsReviews();
+            //boolean variable to store the result of the validation
+            Boolean Ok = false;
+            //create some data to use with the method
+            string Title = "haha";
+            string MainReview = "great";
+            string Score = "3.3";
+            //invoke the method
+            Ok = AReviews.Valid(Title, MainReview, Score);
+            //test tto see that the reuslt is correct
+            Assert.IsTrue(Ok);
+        }
+    }
+
+}
